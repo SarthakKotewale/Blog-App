67aa0fe754f251986833beb1c57810fa0248314e
SarthakKotewale <sarthakk0808@gmail.com>
2024-03-10 11:18:47 +0530

    added getFilePreview service

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -98,12 +98,16 @@ export class Service {
 
   async deleteFile(fileId) {
     try {
-        await this.bucket.deleteFile(conf.appwriteBucketId, fileId)
-        return true;
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
     } catch (error) {
       console.log("Appwrite service :: deleteFile :: error", error);
       return false;
     }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
   }
 }
 
