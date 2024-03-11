1b7d1c470eb67117e3f23545f8d48fe2c24805d0
SarthakKotewale <sarthakk0808@gmail.com>
2024-03-11 11:30:30 +0530

    added

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({})
+const store = configureStore({
+  reducer: {
+    //TODO: add more slices here for posts
+  },
+});
+
+export default store;
